5ae62486774ebe4eaeb6fb851e164980207026d9
byatriz <159158843+byabrito@users.noreply.github.com>
2024-11-12 11:56:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual o melhor jogador de futebol?',
+    'O Lionel Messi foi o melhor jogador do futebol.'
+)
+
+criaCartao(
+    'Louvor',
+    'Qual é a melhor cantora gospel?',
+    'A Valesca MAyssa é uma das melhores cantoras gospel'
+)
+
+criaCartao(
+    'Futsal',
+    'Quem é a melhor jogadora de futsal?',
+    'Amanda Lyssa de Oliveira Crisóstomo'
+)
+
+criaCartao(
+    'A mais escutada atualmente',
+    'Qual o primeiro louvor mais escutado do mundo??',
+    'Não pare - Midian Lima'
+)
